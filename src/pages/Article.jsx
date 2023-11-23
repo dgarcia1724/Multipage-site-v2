6a4a3e24604297f4a2cd6ec30153ab3a9af1adbc
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-22 18:50:37 -0800

    76. fresh start

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,7 +1,22 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
 function Article() {
+  const { id } = useParams();
+  const url = "http://localhost:3000/articles/" + id;
+  const { data: article, isPending, error } = useFetch(url);
+
   return (
     <div>
-      <h1>Article Page</h1>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {article && (
+        <div>
+          <h2>{article.title}</h2>
+          <p>By {article.author}</p>
+          <p>{article.body}</p>
+        </div>
+      )}
     </div>
   );
 }
