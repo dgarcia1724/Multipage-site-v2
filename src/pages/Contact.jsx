7df59835bf27534b7df600e61d5eeff62bb4741f
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-22 20:56:41 -0800

    done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 function Contact() {
+  const queryString = useLocation().search;
+  console.log(queryString);
+
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get("name");
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here...</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
         debitis optio quasi maxime quos? Tempore officiis commodi reprehenderit,
